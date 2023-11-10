851d01c7dac5c5f0f12bd7ee31c297fb00cdedea
therohit777 <rohitpandeyofficial777@gmail.com>
2023-11-11 00:35:36 +0530

    signup added to home route and import file page added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,33 @@
-import Signupbox from './components/signup/Signuppage';
+import React from 'react';
+import "./components/signup/signup.css";
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <>
-      <Signupbox/>
-    </>
+    <section className='Signup'>
+    <div className="logocontainer">
+      Logo or Illustration
+    </div>
+    <div className="signupcontainer">
+      <header>Welcome to <span>Kentron</span></header>
+      <div className="signupbox">
+        <h1>Sign in</h1>
+        <div className="inputs">
+          <p>Organization Name</p>
+          <input type="text" placeholder='Enter organization name' />
+        </div>
+        <div className="inputs">
+          <p>Bussiness Email</p>
+          <input type="email" placeholder='Enter bussiness name' />
+        </div>
+        <div className="inputs">
+          <p>Password</p>
+          <input type="password" placeholder='Enter Password' />
+        </div>
+        <p className='forgot'>Forgot Password?</p>
+        <Link href="/components/Dashboard" className='loginbtn'>Login</Link>
+      </div>
+    </div>
+  </section>
   )
 }
